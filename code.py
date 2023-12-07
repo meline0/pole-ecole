390927cda1b792294fc05f3e4d2127a08f5335d8
meline0 <153198957+meline0@users.noreply.github.com>
2023-12-07 14:36:51 +0100

    Create code.py

---
+++ code.py
@@ -0,0 +1,66 @@
+import sqlite3
+
+#Connexion
+connexion = sqlite3.connect('mynewbase.db')
+
+#Récupération d'un curseur
+c = connexion.cursor()
+
+# ---- début des instructions SQL
+
+#Création de la table
+c.execute("""
+    CREATE TABLE IF NOT EXISTS ecoles (
+    id_ecole INTEGER PRIMARY KEY,
+    nom_ecole TEXT,
+    lien TEXT,
+    description TEXT,
+    adresse TEXT
+    );
+    """)
+c.execute("""
+    CREATE TABLE IF NOT EXISTS jobs (
+    id_job INTEGER PRIMARY KEY,
+    nom_job TEXT,
+    description TEXT,
+    formation TEXT
+    );
+    """)
+c.execute("""
+    CREATE TABLE IF NOT EXISTS domaines (
+    id_domaine INTEGER PRIMARY KEY,
+    nom_domaine TEXT
+    );
+    """)
+c.execute("""
+    CREATE TABLE IF NOT EXISTS proposer (
+    id_domaine INTEGER,
+	id_ecole INTEGER,
+	PRIMARY KEY (id_domaine, id_ecole),
+	FOREIGN KEY (id_domaine) REFERENCES domaines (id_domaine),
+	FOREIGN KEY (id_ecole) REFERENCES ecoles (id_ecole) 
+    );
+    """)
+c.execute("""
+    CREATE TABLE IF NOT EXISTS client (
+    id_client INTEGER PRIMARY KEY,
+	nom TEXT,
+	prenom TEXT,
+	mail TEXT,
+	num INTEGER,
+	date_naissance date,
+	id_ecole INTEGER,
+	id_job INTEGER,
+	FOREIGN KEY (id_job) REFERENCES jobs (id_job),
+	FOREIGN KEY (id_ecole) REFERENCES ecoles (id_ecole) 
+    );
+    """)
+
+# ---- fin des instructions SQL
+
+#Validation
+connexion.commit()
+
+
+#Déconnexion
+connexion.close()
