39db2fa288777ef4be660be82d71d175dd72e383
meline0 <153198957+meline0@users.noreply.github.com>
2023-12-13 11:57:08 +0100

    Début tkinter

--- code.py
+++ code.py
@@ -58,6 +58,37 @@ c.execute("""
 
 # ---- fin des instructions SQL
 
+#Tkinter: fenêtre
+
+fenetre = Tk()
+fenetre.mainloop()
+
+def alert():
+    showinfo("alerte", "Bravo!")
+
+menubar = Menu(fenetre)
+
+menu1 = Menu(menubar, tearoff=0)
+menu1.add_command(label="Créer", command=alert)
+menu1.add_command(label="Editer", command=alert)
+menu1.add_separator()
+menu1.add_command(label="Quitter", command=fenetre.quit)
+menubar.add_cascade(label="Fichier", menu=menu1)
+
+menu2 = Menu(menubar, tearoff=0)
+menu2.add_command(label="Couper", command=alert)
+menu2.add_command(label="Copier", command=alert)
+menu2.add_command(label="Coller", command=alert)
+menubar.add_cascade(label="Editer", menu=menu2)
+
+menu3 = Menu(menubar, tearoff=0)
+menu3.add_command(label="A propos", command=alert)
+menubar.add_cascade(label="Aide", menu=menu3)
+
+fenetre.config(menu=menubar)
+
+#Fin Tkinter
+
 #Validation
 connexion.commit()
 
