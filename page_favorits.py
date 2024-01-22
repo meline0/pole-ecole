17f16b8fc3e3fec807a33984be7dabec24ed692f
Elisa N <117632231+iminiom@users.noreply.github.com>
2024-01-22 18:17:20 +0100

    Update page_favorits.py

--- page_favorits.py
+++ page_favorits.py
@@ -3,28 +3,29 @@ import csv
 import webbrowser
 
 def favorits():
-
-    fenetre_fav = Toplevel()
+    fenetre_fav = Tk()
     fenetre_fav.title("Liste des favorits")
     fenetre_fav.geometry("900x670")
     fenetre_fav.configure(bg='#9c742f')
 
-    
-    text_fav = Text(fenetre_fav, wrap=WORD)
+    text_fav = Text(fenetre_fav, wrap=WORD, bg='#9c742f', fg='white', font=("Arial", 12))
     text_fav.pack(expand=YES, fill=BOTH)
     text_fav.tag_configure("hyperlink", foreground="blue", underline=True)
 
-    # overture du fichier CSV et lecture de son contenu
+    # Ouvrir le fichier CSV et lire son contenu
     with open('favorits.csv', 'r', encoding='latin-1') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
             nom_ecole, adresse_ecole, url_site_ecole = row
 
-           
-            text_fav.insert(END, f"Nom de l'école: {nom_ecole}\n")
+            # Insérer le texte dans le widget Text
+            text_fav.insert(END, f"\nNom de l'école: {nom_ecole}\n")
             text_fav.insert(END, f"Adresse: {adresse_ecole}\n")
             text_fav.insert(END, f"URL du site: {url_site_ecole}\n", "hyperlink")
-            text_fav.tag_bind("hyperlink", "<Button-1>", lambda event, url=url_site_ecole: webbrowser.open_new(url)) # pour que le lien s'ouvre
+            text_fav.insert(END, "\n" + "-" * 50 + "\n")
+
+            # Associer une fonction pour ouvrir le lien dans le navigateur
+            text_fav.tag_bind("hyperlink", "<Button-1>", lambda event, url=url_site_ecole: webbrowser.open_new(url))
 
 favorits()
 mainloop()
