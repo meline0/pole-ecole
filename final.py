97859416f888f91a1b87a88d71b9926293e6dad9
Smith06S <153199704+Smith06S@users.noreply.github.com>
2024-01-27 14:35:27 +0100

    Update final.py

--- final.py
+++ final.py
@@ -821,29 +821,30 @@ def boutonFourreTout():
 
     return(0)
 
-def favorits():
+def favorits():    
     fin = Tk()
     connexion = sqlite3.connect('pole_ecole.db')
     c = connexion.cursor()
-
-    def refresh_window():
-        fin.update()
-        fin.update_idletasks()
-
     ###Le fonction qui permet de supprimer une ligne
-    def cancella(ecole):
-        fichier = open("mail.txt","r")
-        mail = fichier.read()
-        c.execute("SELECT id_client FROM client WHERE mail ="+mail)
-        id = c.fetchall
-        c.execute("DELETE FROM sauvegarde WHERE nom_ecole ='"+ecole+"'AND id_client = "+id)
-        refresh_window()
+    def cancella():
+            selezione = tv.selection()[0]
+            mb_canc = messagebox.askokcancel(
+                title="Confirmer la suppression",
+                message="Voulez-vous confirmer l'élimination?")
+            if mb_canc == True:
+                tv.delete(selezione)
+                """fichier = open("mail.txt","r") 
+                mail = fichier.read()
+                c.execute("SELECT id_client FROM client WHERE mail ="+mail)
+                id = c.fetchall()
+                c.execute("DELETE FROM sauvegarde WHERE nom_ecole ='"+selezione+"'AND id_client = "+id)"""
+
 
     tv = ttk.Treeview(fin,
-                      columns=("Ecole", "Adresse", "URL"),
-                      show='headings')
+                        columns=("Ecole", "Adresse", "URL"),
+                        show='headings')
 
-    ###Les colonnes qu'il y a sur le tkinter
+        ###Les colonnes qu'il y a sur le tkinter
     tv.column('#0')
     tv.column('#1')
     tv.column('#2')
@@ -856,27 +857,18 @@ def favorits():
 
     tv.pack()
 
-    ###Recupère les données de sauvegarde et les mets dans la page tkinter
+        ###Recupère les données de sauvegarde et les mets dans la page tkinter
     c.execute("SELECT nom_ecole,lien,adresse FROM sauvegarde")
-    favoris = c.fetchall
-    for i in range(len(favoris)):
-        eco = favoris[i][0]
-        url = favoris[i][1]
-        adr = favoris[i][2]
-    
+    favoris = c.fetchall()
+    for i in favoris:
+        eco = i[0]
+        url = i[1]
+        adr = i[2]
+        print(eco,url,adr)
+        tv.insert("", 0, values=(adr, eco, url))
 
-    """with open('favorits.csv', 'r+') as elenco:
-        lettore = csv.reader(elenco)
-        for row in lettore:
-            adr = row[0]
-            eco = row[1]
-            url = row[2]"""
-    
-    tv.insert("", 0, values=(adr, eco, url))
-
-    #Met le bouton qui supprime renvoie a la fonction cancella
-    puls_canc = Button(fin, text="Supprimer",
-                       command=cancella(eco))
+        #Met le bouton qui supprime renvoie a la fonction cancella
+    puls_canc = ttk.Button(fin, text="Supprimer", command=cancella)
     puls_canc.pack()
 
 
